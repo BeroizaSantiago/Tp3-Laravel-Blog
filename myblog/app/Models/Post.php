8fe26cd5e44ef62e353920b8ccf8bea38a976d57
LuciaScalia <luciaxscaliax@gmail.com>
2025-06-08 19:49:31 -0300

    migracion y model de post

--- myblog/app/Models/Post.php
+++ myblog/app/Models/Post.php
@@ -13,6 +13,7 @@ class Post extends Model
         'title',
         'content',
         'poster',
+        //'habilitated',
         'category',
         'user_id'
     ];
--- myblog/resources/views/components/post.blade.php
+++ myblog/resources/views/components/post.blade.php
@@ -5,9 +5,11 @@
         <div class="w-full flex justify-center text-xl">
             <p class="p-3 font-bold">¡Todavía no hay posts!</p>
         </div>
-        <div class="flex justify-center">
-            <a href="{{ url('/category/create') }}"><x-button type="button" class="bg-blue-600 hover:bg-blue-700 hover:cursor-pointer text-white m-2 mr-0 px-4 py-2 rounded-full w-24" text="Crear"></x-button></a>
-        </div>
+        @if(Auth::check())
+            <div class="flex justify-center">
+                <a href="{{ url('/category/create') }}"><x-button type="button" class="bg-blue-600 hover:bg-blue-700 hover:cursor-pointer text-white m-2 mr-0 px-4 py-2 rounded-full w-24" text="Crear"></x-button></a>
+            </div>
+        @endif
     @else
         @foreach($posts as $post)
             <div class="border p-6 rounded shadow-lg bg-white relative"> 
